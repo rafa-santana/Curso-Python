e88f8d9068b260116a7482f74e0312bdc0d5315b
Rafael Reis dos Santos Santana <rafaelrss@ufba.br>
2021-07-13 12:28:33 -0300

    Exercícios da Aula 04

---
+++ Aula 04/desafio 02.py
@@ -0,0 +1,4 @@
+dia = input ('Informe o dia do seu nascimento ')
+mes = input ('Informe o mês do seu nascimento ')
+ano = input ('Informe o ano do seu nascimento ')
+print ('Você nasceu no dia',dia,'de', mes, 'de', ano, '. Correto?')
---
+++ Aula 04/desafio 03.py
@@ -0,0 +1,4 @@
+n1 = input ('Digite o primeiro número ')
+n2 = input ('Digite o segundo número ')
+soma = n1 + n2
+print ('A soma entre eles é: ', soma)
